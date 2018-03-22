f4566f83f95ed84f0cadaf414ad63c31e3882387
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-03-22 14:20:49 -0700

    Adding Mozilla drivers. Demo test running.

---
+++ jenkins-e2e/tests/demo.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'Demo test' : function (browser) {
+
+        console.log(browser.globals);
+        // {
+        //   "myGlobalVar" : "some value",
+        //   "otherGlobal" : "some other value"
+        // }
+
+    }
+};
